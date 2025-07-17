e0984ab9ce4807f48ae257f3c4bd7743d78365b7
SociopathEliot <dlaonlcahik@gmail.com>
2025-07-17 12:05:48 +0600

    onBackPressedDispatcher

--- app/src/main/java/be/buithg/etghaifgte/presentation/ui/fragments/main/TutorialFragment.kt
+++ app/src/main/java/be/buithg/etghaifgte/presentation/ui/fragments/main/TutorialFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
 import be.buithg.etghaifgte.R
 import be.buithg.etghaifgte.databinding.FragmentTutorialBinding
 
@@ -21,5 +23,13 @@ class TutorialFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigateUp()
+            }
+        })
+    }
 
 }
