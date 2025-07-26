79c0eba16d0daf8dfcfa203eea7f12cf2360eed2
SociopathEliot <dlaonlcahik@gmail.com>
2025-07-26 15:46:37 +0600

    Finished project

--- app/src/main/java/be/buithg/etghaifgte/data/remote/ApiInterface.kt
+++ app/src/main/java/be/buithg/etghaifgte/data/remote/ApiInterface.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface ApiInterface {
 
-    @GET("currentMatches")
+    @GET("matches")
     suspend fun getLiveScore(@Query("apikey") apikey :String) : CricketData
 
 
